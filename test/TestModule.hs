9e031e34c0bb0c033eb40d7639d7db08d35012cc
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-21 21:43:32 +0200

    add seperate function

--- test/TestModule.hs
+++ test/TestModule.hs
@@ -3,7 +3,10 @@ module TestModule where
 import Properties
 
 basicPlusEasy :: Equiv Integer Integer
-basicPlusEasy = Equiv basicPlus (+ 15) 
+basicPlusEasy = Equiv basicPlus easyPlus
+
+easyPlus :: Integer -> Integer
+easyPlus a = 15 + a
 
 basicPlus :: Integer -> Integer
 basicPlus input = 10 + fiveConstant + input
