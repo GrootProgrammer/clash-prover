f19cb5654f895969069f321da21977b6df4f5cd8
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-21 21:43:24 +0200

    use PreFixOf and make it smarter

--- src/Properties.hs
+++ src/Properties.hs
@@ -7,6 +7,7 @@ module Properties (
 
 import Data.Data
 import CoreTranslate.Language
+import Data.List (isPrefixOf)
 
 data Equiv input result = Equiv (input -> result) (input -> result) deriving (Typeable)
 
@@ -15,6 +16,7 @@ data EquivCond input result = EquivCond (input -> Bool) (input -> result) (input
 findEquivInLanguage :: ProveLanguage -> [(ProveExpression, ProveExpression)]
 findEquivInLanguage = concatMap findEquivInDef
 
+-- as Equiv has 2 (type) parameters and 2 args it becomes Def _ (DO (DO (DO (DO EquivString) type1) type2) arg1) arg2) where type1 == input and type2 == output
 findEquivInDef :: VariableDef -> [(ProveExpression, ProveExpression)]
-findEquivInDef (Def _ (DirectOperation (DirectOperation (DirectOperation (DirectOperation (Variable equivName _) _ _) _ _) arg2 _) arg1 _)) = [(arg1, arg2)]
+findEquivInDef (Def _ (DirectOperation (DirectOperation (DirectOperation (DirectOperation (Variable equivName _) _ _) _ _) arg1 _) arg2 _)) = [(arg1, arg2) | "$my-prover-0.1.0.0-inplace$Properties$Equiv" `isPrefixOf` show equivName ]
 findEquivInDef _ = []
