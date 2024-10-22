a46b9fb8da384f87673b6f92c38e63a30f4bd442
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-22 11:14:51 +0200

    add Equivalence testing

--- src/CoreTranslate/Language.hs
+++ src/CoreTranslate/Language.hs
@@ -1,11 +1,13 @@
 
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE InstanceSigs #-}
 module CoreTranslate.Language where
 import GHC.Core.TyCo.Rep
 import GHC.Plugins hiding (Case)
+import GHC.Core.TyCo.Compare (tcEqKind)
 
 data LiteralTypes = LChar Char | LNumber Integer | LString String | LFloat Rational | LDouble Rational | Typed ProveType
-    deriving (Show)
+    deriving (Show, Eq)
 
 data ProveExpression    = Literal           { lt :: LiteralTypes, pt :: ProveType}
                         | Variable          { name :: ProveName, pt :: ProveType }
@@ -13,13 +15,13 @@ data ProveExpression    = Literal           { lt :: LiteralTypes, pt :: ProveTyp
                         | Lambda            { name :: ProveName, expr :: ProveExpression, pt :: ProveType }
                         | Case              { options :: [CaseInstance], pt :: ProveType }
                         | DirectOperation   { f :: ProveExpression, arg :: ProveExpression, pt :: ProveType }
-    deriving (Show)
+    deriving (Show, Eq)
 
 data CaseInstance   = CI ProveExpression ProveExpression
-    deriving (Show)
+    deriving (Show, Eq)
 
 data VariableDef = Def { defName :: ProveName, defExpr :: ProveExpression}
-    deriving (Show)
+    deriving (Show, Eq)
 
 type ProveLanguage  = [VariableDef]
 
@@ -31,3 +33,6 @@ instance Show ProveName where
     show (PN v) = nameStableString (getName v) ++ "(" ++ show (getUnique $ getName v) ++ ")"
 instance Show ProveType where
     show _ = "TypeInfo"
+instance Eq ProveType where
+  (==) :: ProveType -> ProveType -> Bool
+  (==) (PT kind1) (PT kind2) = tcEqKind kind1 kind2
