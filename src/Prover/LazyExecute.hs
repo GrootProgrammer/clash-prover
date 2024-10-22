62657b2b52e548e36b17d08be5182a2f18577235
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-22 11:02:29 +0200

    remove most LazyExecute

--- src/Prover/LazyExecute.hs
+++ src/Prover/LazyExecute.hs
@@ -3,17 +3,3 @@ module Prover.LazyExecute where
 
 import CoreTranslate.Language
 import Prelude hiding (lookup)
-
---type Conditionals = [ProveExpression]
---
---lookup :: ProveLanguage -> ProveName -> ProveType -> ProveExpression
---lookup dict name _ = defExpr $ head $ filter (\i -> defName i == name) dict 
---
---lazyExecution :: ProveLanguage -> ProveExpression -> Conditionals -> [ProveExpression]
---lazyExecution _     (Literal lt pt) cond = [Literal lt pt]
---lazyExecution _     (Symbolic s e pt) cond = [Symbolic s e pt]
---lazyExecution dict  (Variable s pt) cond = [lookup dict s pt]
---lazyExecution TypeVar        
---lazyExecution Lambda         
---lazyExecution Case           
---lazyExecution DirectOperation
