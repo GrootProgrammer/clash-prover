c52057afba0ff4fd58c17173c61a2d6155d46a44
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-23 21:01:49 +0200

    add load helpers

--- src/CoreTranslate/LanguageUtils.hs
+++ src/CoreTranslate/LanguageUtils.hs
@@ -1,4 +1,20 @@
 module CoreTranslate.LanguageUtils where
 
 import CoreTranslate.Language
+import GHC.Plugins
+import GHC.Prelude
+import CoreTranslate.Translate
 
+isPrimitive :: VariableDef -> Bool
+isPrimitive (Def n1 (Variable n2 _)) = n1 == n2
+isPrimitive _ = False 
+
+--panics if ProveName is in the current Module
+getExternalVariableDef :: ProveName -> VariableDef
+getExternalVariableDef (PN id) = Def (PN id) (convertExpression $ unfoldingTemplate $ realIdUnfolding id)
+
+getVariableDef :: ProveLanguage -> ProveName -> Maybe VariableDef
+getVariableDef (x:xs) n
+    | defName x == n = Just x
+    | otherwise = getVariableDef xs n
+getVariableDef [] _ = Nothing
