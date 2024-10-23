5b9c457c8c00cf90a2c6932713fed95d0e177510
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-23 21:03:58 +0200

    change CorePrintPlugin

--- src/CorePrintPlugin.hs
+++ src/CorePrintPlugin.hs
@@ -5,21 +5,13 @@ module CorePrintPlugin (plugin) where
 import Prelude
 import GHC.Plugins
 import CoreTranslate.Translate
-import Data.List (intercalate)
 import Properties (findEquivInLanguage)
 import CoreTranslate.Language
-import GHC (GhcMonad(..), LoadHowMuch (LoadAllTargets))
-import GHC.Utils.Exception (ExceptionMonad)
-import GHC.Driver.Make
-import GHC.Driver.Monad
-import Control.Monad.Catch
 
 plugin :: Plugin
 plugin = defaultPlugin {
   installCoreToDos = install
 }
-
-type CoreGHC = Ghc (CoreM ())
 
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 install _ todo = do
@@ -28,10 +20,9 @@ install _ todo = do
 pass :: ModGuts -> CoreM ModGuts
 pass guts = do
   --d <- CoreTranslate.Unfolding.lookup n_1
-  liftIO $ print $ (show n_1)
+  liftIO $ putStrLn $ show $ convertExpression $ unfoldingTemplate $ realIdUnfolding  n_1
   return guts
       where
-        ghcInstance = liftGhcT @CoreM
-        (Variable n_1 _, equiv_1_2) = head equivs
+        (Variable (PN n_1) _, equiv_1_2) = head equivs
         equivs = findEquivInLanguage lang
         lang = convertBinds $ mg_binds guts
