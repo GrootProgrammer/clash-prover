574e9d3d43961e725a7da28bfd9855545eb82257
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-11-12 19:55:37 +0100

    fix warnings on Translate

--- src/CoreTranslate/Translate.hs
+++ src/CoreTranslate/Translate.hs
@@ -2,15 +2,15 @@
 module CoreTranslate.Translate(convertBinds, convertExpression, replaceVariable, getExternalVariableDef) where
 
 import Prelude
-import CoreTranslate.Language hiding (arg, name, expr)
+import CoreTranslate.Language
 import GHC.Plugins
 
 convertBinds :: [CoreBind] -> ProveLanguage
 convertBinds = concatMap convertBind
 
 convertBind :: CoreBind -> ProveLanguage
-convertBind (NonRec id expr) = [convertIdExpression id expr]
-convertBind (Rec l) = concatMap (\(id, expr) -> convertBind (NonRec id expr)) l
+convertBind (NonRec _id expr) = [convertIdExpression _id expr]
+convertBind (Rec l) = concatMap (\(_id, expr) -> convertBind (NonRec _id expr)) l
 
 convertIdExpression :: CoreBndr -> CoreExpr -> VariableDef
 convertIdExpression name expr = Def (PN name) (convertExpression expr)
@@ -32,7 +32,7 @@ convertExpression (Var info)                            = Variable (PN info)
 convertExpression (Lit lit)                             = Literal (convertLiteral lit)
 convertExpression (App expr arg)                        = DirectOperation (convertExpression expr) (convertExpression arg)
 convertExpression (Lam varToBind expression)            = Lambda (PN varToBind) (convertExpression expression)
-convertExpression (Let (NonRec id expr) expression)     = replaceVariable (convertExpression expression) (PN id) (convertExpression expr)
+convertExpression (Let (NonRec _id expr) expression)     = replaceVariable (convertExpression expression) (PN _id) (convertExpression expr)
 convertExpression (GHC.Plugins.Case a b _ d)            = CoreTranslate.Language.Case (convertExpression a) (PN b) (fmap convertAlt d)
 convertExpression (Cast e _)                            = convertExpression e
 convertExpression (Tick _ expr)                         = convertExpression expr
@@ -43,18 +43,18 @@ convertExpression t                                     = error $ "non exhaustiv
 convertAlt :: Alt CoreBndr -> CaseInstance
 convertAlt (Alt con bind expr) = CI con (fmap PN bind) (convertExpression expr)
 
-convertNumType :: LitNumType -> NumTypes 
-convertNumType GHC.Plugins.LitNumBigNat = CoreTranslate.Language.LitNumBigNat 
-convertNumType GHC.Plugins.LitNumInt    = CoreTranslate.Language.LitNumInt    
-convertNumType GHC.Plugins.LitNumInt8   = CoreTranslate.Language.LitNumInt8   
-convertNumType GHC.Plugins.LitNumInt16  = CoreTranslate.Language.LitNumInt16  
-convertNumType GHC.Plugins.LitNumInt32  = CoreTranslate.Language.LitNumInt32  
-convertNumType GHC.Plugins.LitNumInt64  = CoreTranslate.Language.LitNumInt64  
-convertNumType GHC.Plugins.LitNumWord   = CoreTranslate.Language.LitNumWord   
-convertNumType GHC.Plugins.LitNumWord8  = CoreTranslate.Language.LitNumWord8  
-convertNumType GHC.Plugins.LitNumWord16 = CoreTranslate.Language.LitNumWord16 
-convertNumType GHC.Plugins.LitNumWord32 = CoreTranslate.Language.LitNumWord32 
-convertNumType GHC.Plugins.LitNumWord64 = CoreTranslate.Language.LitNumWord64 
+convertNumType :: LitNumType -> NumTypes
+convertNumType GHC.Plugins.LitNumBigNat = CoreTranslate.Language.LitNumBigNat
+convertNumType GHC.Plugins.LitNumInt    = CoreTranslate.Language.LitNumInt
+convertNumType GHC.Plugins.LitNumInt8   = CoreTranslate.Language.LitNumInt8
+convertNumType GHC.Plugins.LitNumInt16  = CoreTranslate.Language.LitNumInt16
+convertNumType GHC.Plugins.LitNumInt32  = CoreTranslate.Language.LitNumInt32
+convertNumType GHC.Plugins.LitNumInt64  = CoreTranslate.Language.LitNumInt64
+convertNumType GHC.Plugins.LitNumWord   = CoreTranslate.Language.LitNumWord
+convertNumType GHC.Plugins.LitNumWord8  = CoreTranslate.Language.LitNumWord8
+convertNumType GHC.Plugins.LitNumWord16 = CoreTranslate.Language.LitNumWord16
+convertNumType GHC.Plugins.LitNumWord32 = CoreTranslate.Language.LitNumWord32
+convertNumType GHC.Plugins.LitNumWord64 = CoreTranslate.Language.LitNumWord64
 
 convertLiteral :: Literal -> LiteralTypes
 convertLiteral (LitChar c)
@@ -70,14 +70,14 @@ convertLiteral (LitDouble d)
 
 --panics if ProveName is in the current Module
 getExternalVariableDef :: [ProveExpression] -> ProveName -> VariableDef
-getExternalVariableDef _ (PN id) = case unfolding of
-    NoUnfolding -> Def (PN id) (Variable (PN id))
-    BootUnfolding -> Def (PN id) (Variable (PN id))
-    (OtherCon []) -> Def (PN id) (Literal (Constructor (PN id)))
-    (OtherCon [DataAlt l]) -> Def (PN id) (Literal (Dict l [Variable (PN id)]))
+getExternalVariableDef _ (PN _id) = case unfolding of
+    NoUnfolding -> Def (PN _id) (Variable (PN _id))
+    BootUnfolding -> Def (PN _id) (Variable (PN _id))
+    (OtherCon []) -> Def (PN _id) (Literal (Constructor (PN _id)))
+    (OtherCon [DataAlt l]) -> Def (PN _id) (Literal (Dict l [Variable (PN _id)]))
     (OtherCon xs) -> error ("constructor with multiple options: " ++ showPprUnsafe (OtherCon xs))
-    (DFunUnfolding [] c args) -> Def (PN id) (Literal (Dict c (fmap convertExpression args)))
-    (DFunUnfolding vars c args) -> Def (PN id) (foldr (\v e -> Lambda (PN v) e) (Literal (Dict c (fmap convertExpression args))) vars)
-    (CoreUnfolding expr _ _ _ _) -> Def (PN id) (convertExpression expr)
+    (DFunUnfolding [] c args) -> Def (PN _id) (Literal (Dict c (fmap convertExpression args)))
+    (DFunUnfolding vars c args) -> Def (PN _id) (foldr (Lambda . PN) (Literal (Dict c (fmap convertExpression args))) vars)
+    (CoreUnfolding expr _ _ _ _) -> Def (PN _id) (convertExpression expr)
     where
-        unfolding = realIdUnfolding id
+        unfolding = realIdUnfolding _id
